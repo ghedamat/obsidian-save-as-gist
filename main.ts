c66413411cda9c0df517e1b321ceb69866c341fc
Mattia Gheda <ghedamat@gmail.com>
2022-01-16 10:59:46 -0500

    initial version

--- main.ts
+++ main.ts
@@ -1,81 +1,57 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting} from 'obsidian';
+import {Octokit} from '@octokit/rest';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface SaveAsGistSettings {
+	githubApiToken?: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: SaveAsGistSettings = {
+	githubApiToken: null
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class SaveAsGist extends Plugin {
+	settings: SaveAsGistSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+			id: 'save-as-new-gist',
+			name: 'Save current file as a new private Gist',
+			callback: async () => {
+				//new SampleModal(this.app).open();
+				const noteFile = this.app.workspace.getActiveFile(); // Currently Open Note
+				const fileName = noteFile.name;
+				if (!fileName) {
+					return; // Nothing Open
 				}
+
+				// Read the currently open note file.
+				let body = await this.app.vault.read(noteFile);
+
+				await this._saveAsGist(fileName, body);
+			}
+		});
+
+		this.addCommand({
+			id: 'save-as-new-gist-selection',
+			name: 'Save current selection as a new private Gist',
+			editorCallback: (editor: Editor, _view: MarkdownView) => {
+				const noteFile = this.app.workspace.getActiveFile(); // Currently Open Note
+				const fileName = noteFile.name;
+				if (!fileName) {
+					return; // Nothing Open
+				}
+				const body = editor.getSelection();
+
+				this._saveAsGist(fileName, body);
 			}
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new SaveAsGistSettingTab(this.app, this));
 	}
 
 	onunload() {
@@ -89,28 +65,44 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	async _saveAsGist(fileName: string, body: string) {
+		const token = this.settings.githubApiToken;
+
+		if (!token) {
+			new Notice('GitHub token not found, check your settings');
+			return;
+		}
+		try {
+			const octokit = new Octokit({
+				auth: token
+			});
+
+			const result = await octokit.rest.gists.create({
+				files: {
+					[fileName]: {
+						content: body
+					}
+				}
+			})
+
+			const url = result.data.html_url;
+			await navigator.clipboard.writeText(url);
+
+			new Notice(`Gist created ${url} - URL copied to your clipboard`);
+		} catch (err) {
+			new Notice('There was an error creating your gist, check your token and connection');
+			throw err;
+		}
+
+	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+class SaveAsGistSettingTab extends PluginSettingTab {
+	plugin: SaveAsGist;
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: SaveAsGist) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -120,17 +112,16 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'Settings for Save As Gist'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Github API token')
+			.setDesc('create a token here https://github.com/settings/tokens/new (only gist permission required)')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter your Github API token')
+				.setValue(this.plugin.settings.githubApiToken)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.githubApiToken = value;
 					await this.plugin.saveSettings();
 				}));
 	}
